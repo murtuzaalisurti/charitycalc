94cc711bf77f1da22fcb8a83352e21a28cda6346
Murtuza <68743212+murtuzaalisurti@users.noreply.github.com>
2021-01-01 21:41:24 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelector("#actualincome").disabled = true;
-    document.querySelector("#principalamount").onkeyup = () => {
+    document.getElementById("inflationrate").onkeyup = () => {
         document.querySelector("#actualincome").disabled = false;
         document.querySelector("form").onsubmit = () => {
             class Donate {
@@ -10,9 +10,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     this.inflation = inflation;
                 }
                 calcincome() {
+                    console.log(typeof this.interest);
                     if (this.interest > 0) {
+                        console.log(typeof this.interest);
                         var result = document.querySelector("#output");
-                        var pminusint = this.principal - this.interest;
+                        var pminusint = this.principal - (this.interest);
+                        console.log(typeof pminusint);
                         var rate = (this.interest * 100) / this.principal;
                         if (rate >= this.inflation) {
                             var actualinc = (pminusint + ((this.inflation * pminusint) / 100));
@@ -29,55 +32,22 @@ document.addEventListener("DOMContentLoaded", function () {
                             console.log(actualincround);
                             result.innerHTML = `Your actual income is&nbsp<b>${actualincround}</b>&nbspand you have to donate&nbsp<b>${charityround}</b>`;
                         }
-                    } else {
+                    } else if (this.interest == 0) {
+                        var result = document.querySelector("#output");
                         result.innerHTML = `Your actual income is&nbsp<b>${this.principal}</b>`;
+                    }
+                    else {
+                        var result = document.querySelector("#output");
+                        result.innerHTML = "Something's wrong. Please enter a valid number and try again.";
                     }
                 }
             }
-            var principalamount = document.querySelector("#principalamount").value;
-            var interestamount = document.querySelector("#interestamount").value;
-            var inflationrate = document.querySelector("#inflationrate").value;
+            var principalamount = (document.querySelector("#principalamount").value);
+            var interestamount = (document.querySelector("#interestamount").value);
+            var inflationrate = (document.querySelector("#inflationrate").value);
             var incomecalculate = new Donate(principalamount, interestamount, inflationrate).calcincome();
             document.querySelector("#actualincome").disabled = true;
             return false;
         }
     }
-    // document.querySelector("form").onsubmit = () => {
-    //     class Donate {
-    //         constructor(principal, interest, inflation) {
-    //             this.principal = principal;
-    //             this.interest = interest;
-    //             this.inflation = inflation;
-    //         }
-    //         calcincome() {
-    //             if (this.interest > 0) {
-    //                 var result = document.querySelector("#output");
-    //                 var pminusint = this.principal - this.interest;
-    //                 var rate = (this.interest * 100) / this.principal;
-    //                 if (rate >= this.inflation) {
-    //                     var actualinc = (pminusint + ((this.inflation * pminusint) / 100));
-    //                     var actualincround = actualinc.toFixed(2);
-    //                     var charity = (this.principal - actualinc);
-    //                     var charityround = charity.toFixed(2);
-    //                     console.log(actualincround);
-    //                     result.innerHTML = `Your actual income is&nbsp<b>${actualincround}</b>&nbspand you have to donate&nbsp<b>${charityround}</b>`;
-    //                 } else {
-    //                     var actualinc = pminusint;
-    //                     var actualincround = actualinc.toFixed(2);
-    //                     var charity = this.principal - actualinc;
-    //                     var charityround = charity.toFixed(2);
-    //                     console.log(actualincround);
-    //                     result.innerHTML = `Your actual income is&nbsp<b>${actualincround}</b>&nbspand you have to donate&nbsp<b>${charityround}</b>`;
-    //                 }
-    //             } else {
-    //                 result.innerHTML = `Your actual income is&nbsp<b>${this.principal}</b>`;
-    //             }
-    //         }
-    //     }
-    //     var principalamount = document.querySelector("#principalamount").value;
-    //     var interestamount = document.querySelector("#interestamount").value;
-    //     var inflationrate = document.querySelector("#inflationrate").value;
-    //     var incomecalculate = new Donate(principalamount, interestamount, inflationrate).calcincome();
-    //     return false;
-    // }
 });
